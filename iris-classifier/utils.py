d3fc7278837d224e03479494bae2ee8cd087f083
Purva Shinde <purvapshinde.21@gmail.com>
2025-12-17 21:45:18 +0530

    Refactor load_model to return model directly
    Simplified the load_model function by removing an unnecessary variable.

--- iris-classifier/utils.py
+++ iris-classifier/utils.py
@@ -1,11 +1,15 @@
 import joblib
 import numpy as np
 
+
 def load_model(model_path="model.joblib"):
-    model = joblib.load(model_path)
-    return model
+return joblib.load(model_path)
 
 def predict_species(model, features):
     prediction = model.predict([features])[0]
     return int(prediction)   # 👈 convert numpy.int64 → Python int
+
+
+
+
 
